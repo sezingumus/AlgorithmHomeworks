6332744bbf74fa7129d5781352ad1b591808b275
sezingumus <sezingumus@gmail.com>
2018-06-28 17:31:58 +0300

    HW3 was added

---
+++ HW3/TSP.py
@@ -0,0 +1,119 @@
+"""
+Sezin Gümüş     150113841
+Ufuk  Çetinkaya 150113824
+"""
+import math
+#Each city object has x , y coordinates and neighbors
+class City(object):     
+    def __init__(self , ID,x_coordinate,y_coordinate):
+        self.ID = ID
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.neighbors = []
+ 
+    def addNeighbors(self,ID,distance):
+        self.neighbors.append([ID,distance])
+    def getNeighbors(self):
+        return self.neighbors
+#We import cities from entered input file and find and bound neighbors to each cities.      
+def fileToList(input_file):
+    file = open(input_file,"r")
+    list_of_cities = []
+    line = file.readline()
+    while line:
+        _id,x,y = line.split()
+        list_of_cities.append(City(_id,int(x),int(y)))
+        line = file.readline()
+    file.close()
+    for i in range(len(list_of_cities)):
+        print ("cities:",list_of_cities[i].ID)
+    return boundNeighborsToACity(list_of_cities)
+# This method calculates distance between two cities .
+def calculateDistance(x1,x2,y1,y2):
+        x = (x1-x2) * (x1-x2)
+        y = (y1-y2) * (y1-y2) 
+        d = math.sqrt(x+y) 
+        d = round(d)
+        return d
+#This function bounds neighbors to cities.     
+def boundNeighborsToACity(list_of_cities):
+ length = len(list_of_cities)     
+ for i in range(0,length):
+     for j in range(0,length):
+        distance = calculateDistance(list_of_cities[j].x_coordinate, list_of_cities[i].x_coordinate ,list_of_cities[j].y_coordinate, list_of_cities[i].y_coordinate)
+        if distance != 0:
+            list_of_cities[i].addNeighbors(list_of_cities[j].ID,distance)
+            print (list_of_cities[i].ID,"n[0]",list_of_cities[i].getNeighbors()[0])
+ return list_of_cities
+ #This function finds minimum path between cities and returns path and min cost of the tour .
+def nearestNeighbor (list_of_cities):
+   path = []
+   min_edge = 0
+   total_cost = 0
+   current_city = list_of_cities[0]
+   path.append([current_city.ID,min_edge])
+   next_city_id , min_edge = min(current_city.getNeighbors(),key=lambda x: x[1])
+   next_city_index  = findCity(next_city_id,list_of_cities)
+   next_city = list_of_cities[next_city_index]
+   total_cost += min_edge 
+   print(current_city.ID,"-->",next_city.ID)
+   prev_city = current_city
+   removeNeighbor(prev_city.ID,list_of_cities)
+   current_city = next_city
+   
+   while (current_city.getNeighbors()):
+       
+       path.append([current_city.ID,min_edge])
+       next_city_id , min_edge = min(current_city.getNeighbors(),key=lambda x: x[1])
+       print (next_city_id,":",min_edge)
+       next_city_index  = findCity(next_city_id,list_of_cities)
+       next_city = list_of_cities[next_city_index]
+       print(current_city.ID,"-->",next_city.ID)
+       prev_city = current_city
+       removeNeighbor(prev_city.ID,list_of_cities)
+       total_cost += min_edge
+       current_city = next_city
+       
+   path.append([next_city.ID,min_edge])
+   index = findCity(next_city.ID,list_of_cities)
+   last_city = list_of_cities[index]
+   last_edge = calculateDistance(list_of_cities[0].x_coordinate,last_city.x_coordinate,list_of_cities[0].y_coordinate,last_city.y_coordinate)
+   total_cost += last_edge 
+   return total_cost , path
+#This function finds index of a city in array of "list_of_cities" .               
+def findCity (c,city_list):
+    for i in range(len(city_list)):
+        if city_list[i].ID == c:
+            return i
+    return -1
+#This function finds index of neighbor in all cities array of "list_of_cities" .  
+def findNeighbor(n, neighbor):
+    for i in range(len(neighbor)):
+        n_id , dist = neighbor[i]
+        if n_id == n :
+          return i
+    return -1
+#This function removes visited city from neighbor of all cities .
+def removeNeighbor(n,list_of_cities):
+    for i in range(len(list_of_cities)):
+        neighbor = list_of_cities[i].getNeighbors()
+        j = findNeighbor(n,neighbor)
+        if j != -1:  """if statement is added , if the index is not out of the range of our list ,
+                          neighbor will be deleted """
+         del neighbor[j]
+# This function writes cost of minimum path and the minimum tour .   
+def listtoFile(output_file,cost,path):
+ file = open(output_file,"w")
+ file.write(str(cost))
+ file.write("\n")
+ file.writelines(["%s\n" % item[0] for item in path ])
+ file.close()
+    
+
+cities = fileToList("test-input-3.txt")
+total_cost,path = nearestNeighbor(cities)
+print("Total cost :",total_cost)
+listtoFile("output3.txt",total_cost,path)
+
+    
+   
---
+++ HW3/untitled0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 18 23:19:10 2018
+
+@author: eagle
+"""
+
+a = [3,4,5,7,8,1,1,2,5,7]
+print(a[-1])
